f28807600ed516c7a6f0055eb8d5a451ae4a073c
ksripath <ksripath@tibco.com>
2018-03-08 18:39:22 -0800

    Closures Explanation

--- closures.js
+++ closures.js
@@ -5,11 +5,16 @@ function fo() {
   }
 }
 
-
+//Every time fo() is called new i, and function are created. This new anonymous functions reference is assigned to f.
 var f = fo();
-var a = f(15); // value:  15              
-var b = fo()(15); // value:  15            
-var c = fo()(20); // value:  20            
+//Because of the closure f has access to i and is currently 0
+var a = f(15); // value:  15
+//Now a new i and new anonymous function get created because fo() is called again. So i is currently 0
+var b = fo()(15); // value:  15
+//Again a new instance of i and anonymous function are created. So i is still 0
+var c = fo()(20); // value:  20
+//No new function or i are created. f still holds the reference to the previous anonymous function.
+//Because of the closure the anonymous function referenced by f still has access to i. Since i was incremented before, i value is 1 now.
 var d = f(20); // value: 21
 
 console.log(a);
